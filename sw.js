6081d57870f842f59441fda613d8995e3f2b62cf
codeguy <andywong0425@gmail.com>
2026-05-23 01:00:18 +0800

    feat: add 富盛大廈停車場

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "parking-hk-v8";
+const CACHE = "parking-hk-v9";
 const FILES = ["/", "/index.html", "/style.css", "/app.js", "/data/carparks.json", "/manifest.json"];
 
 self.addEventListener("install", (e) => {
